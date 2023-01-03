59a38fb5b444bd0dc168758e645c5deed5d99877
Sara Nooryar <103105418+SaraNooryar@users.noreply.github.com>
2023-01-03 12:44:08 +0430

    initial commit

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -22,9 +22,12 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                SizedBox(
-                  height: 60,
-                  child: Image.asset('images/logo.png'),
+                Hero(
+                  tag: 'logo',
+                  child: SizedBox(
+                    height: 60,
+                    child: Image.asset('images/logo.png'),
+                  ),
                 ),
                 Text(
                   'Flash Chat',
